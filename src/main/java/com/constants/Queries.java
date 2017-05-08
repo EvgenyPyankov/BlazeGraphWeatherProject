764d6ea071e708760c26481a61f19861cdb6f193
Евгений <Sleepwalker17@mail.ru>
2017-05-08 13:12:56 +0300

    Fixed query obtaining all stations

--- src/main/java/com/constants/Queries.java
+++ src/main/java/com/constants/Queries.java
@@ -2,7 +2,7 @@ package com.constants;
 
 public class Queries {
 
-//    public static final String GET_ALL_STATIONS_WITH_DATA_QUERY = "select ?label ?lat ?lng ?alt ?station\n" +
+    //    public static final String GET_ALL_STATIONS_WITH_DATA_QUERY = "select ?label ?lat ?lng ?alt ?station\n" +
 //            "where{\n" +
 //            "  ?station rdf:type ispra:MeasureStation.\n" +
 //            "  ?station schema:location ?location.\n" +
@@ -10,21 +10,69 @@ public class Queries {
 //            "  ?location schema:geo [schema:latitude ?lat; schema:longitude ?lng; geo:alt ?alt].\n" +
 //            "  filter (exists{?s mto:st_measure ?station.}).\n" +
 //            "  }\n";
-    public static final String GET_ALL_STATIONS_WITH_DATA_QUERY="select ?label ?lat ?lng ?alt ?station ?regionLabel ?area ?region\n" +
-        "where{\n" +
-        "  ?station rdf:type ispra:MeasureStation.\n" +
-        "  ?station schema:location ?location.\n" +
-        "  ?location rdfs:label ?label.\n" +
-        "  ?location schema:geo [schema:latitude ?lat; schema:longitude ?lng; geo:alt ?alt].\n" +
-        "  ?location <http://example.org/meteo_ru_data/ontology/federalSubject> ?region \n" +
-        "  filter (exists{?s mto:st_measure ?station.}).\n" +
-        "  service <http://dbpedia.org/sparql>\n" +
-        "    {\n" +
-        "    ?region rdfs:label ?regionLabel .\n" +
-        "    ?region dbp:areaKm ?area .\n" +
-        "    }\n" +
-        "    filter(lang(?regionLabel) = 'en')\n" +
-        " }";
+//    public static final String GET_ALL_STATIONS_WITH_DATA_QUERY="select distinct ?label ?lat ?lng ?alt ?station ?regionLabel ?area ?region\n" +
+//        "where{\n" +
+//        "  ?station rdf:type ispra:MeasureStation.\n" +
+//        "  ?station schema:location ?location.\n" +
+//        "  ?location rdfs:label ?label.\n" +
+//        "  ?location schema:geo [schema:latitude ?lat; schema:longitude ?lng; geo:alt ?alt].\n" +
+//        "  ?location <http://example.org/meteo_ru_data/ontology/federalSubject> ?region \n" +
+//        "  filter (exists{?s mto:st_measure ?station.}).\n" +
+//        "  service <http://dbpedia.org/sparql>\n" +
+//        "    {\n" +
+//        "    ?region rdfs:label ?regionLabel .\n" +
+//        "    ?region dbp:areaKm ?area .\n" +
+//        "    }\n" +
+//        "    filter(lang(?regionLabel) = 'en')\n" +
+//        " }";
+
+
+
+
+//    select distinct ?label ?lat ?lng ?alt ?station ?regionLabel ?area ?region
+//            where
+//    {
+//      ?station rdf:type ispra:MeasureStation.
+//            ?station schema:location ?location.
+//            ?location rdfs:label ?label.
+//            ?location schema:geo [schema:latitude ?lat; schema:longitude ?lng; geo:alt ?alt].
+//        optional
+//        {
+//            ?location <http://example.org/meteo_ru_data/ontology/federalSubject> ?region.
+//            optional
+//            {
+//                service <http://dbpedia.org/sparql>
+//                {
+//   						 ?region rdfs:label ?regionLabel .
+//                        ?region dbp:areaKm ?area .
+//                }
+//                filter(lang(?regionLabel) = 'en')
+//            }
+//        }
+//        filter (exists{?s mto:st_measure ?station.}).
+//    }
+    public static final String GET_ALL_STATIONS_WITH_DATA_QUERY = "select distinct ?label ?lat ?lng ?alt ?station ?regionLabel ?area ?region\n" +
+            "where\n" +
+            "{\n" +
+            "      ?station rdf:type ispra:MeasureStation.\n" +
+            "      ?station schema:location ?location.\n" +
+            "      ?location rdfs:label ?label.\n" +
+            "      ?location schema:geo [schema:latitude ?lat; schema:longitude ?lng; geo:alt ?alt].\n" +
+            "      optional \n" +
+            "  \t  {\n" +
+            "            ?location <http://example.org/meteo_ru_data/ontology/federalSubject> ?region.\n" +
+            "            optional\n" +
+            "        \t{\n" +
+            "            \t\tservice <http://dbpedia.org/sparql>\n" +
+            "   \t\t\t\t\t {\n" +
+            "   \t\t\t\t\t\t ?region rdfs:label ?regionLabel .\n" +
+            "   \t\t\t\t\t\t ?region dbp:areaKm ?area .\n" +
+            "      \t\t\t\t }\n" +
+            "             filter(lang(?regionLabel) = 'en')\n" +
+            "             }\n" +
+            "      }\n" +
+            "          filter (exists{?s mto:st_measure ?station.}).\n" +
+            "}";
     public static final String PREFIX = "prefix geo:   <http://www.w3.org/2003/01/geo/wgs84_pos#> \n" +
             "prefix dbo:   <http://dbpedia.org/ontology/> \n" +
             "prefix dbr:   <http://dbpedia.org/resource/> \n" +
@@ -71,7 +119,7 @@ public class Queries {
 //            "group by (year(?date) as ?year)" +
 //            "order by (?year)";
 
-    public static final String GET_MEAN_TEMP_BY_YEARS_QUERY="select ?year (avg(?o) as ?average)\n" +
+    public static final String GET_MEAN_TEMP_BY_YEARS_QUERY = "select ?year (avg(?o) as ?average)\n" +
             "where{\n" +
             "  values ?value{\n" +
             "  %s\n" +
@@ -81,10 +129,10 @@ public class Queries {
             "  ?s mto:st_measure ?value.\n" +
             "filter(?o >\"-100\"^^xsd:float).\n" +
             "  }\n" +
-            "group by (year(?date) as ?year)"+
+            "group by (year(?date) as ?year)" +
             "order by (?year)";
 
-    public static final String GET_DAY_MEASURE="select ?mean ?max ?min ?date\n" +
+    public static final String GET_DAY_MEASURE = "select ?mean ?max ?min ?date\n" +
             "where{\n" +
             "  ?s mto:tmean ?mean.\n" +
             "  ?s mto:tmax ?max.\n" +
@@ -94,7 +142,7 @@ public class Queries {
             "  filter (?date = \"%s\"^^xsd:date).\n" +
             "}\n";
 
-    public static final String GET_MEAN_TEMP_BY_DAYS_OF_THE_MONTH="select ?date ?mean\n" +
+    public static final String GET_MEAN_TEMP_BY_DAYS_OF_THE_MONTH = "select ?date ?mean\n" +
             "where{\n" +
             "  ?measure mto:tmean ?mean.\n" +
             "  ?measure mto:datem ?date.\n" +
